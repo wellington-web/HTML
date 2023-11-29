4aa409f3f6a76566edbb0ce6270d04e3eda24d10
Wellington <wellingtonTgertrudes@gmail.com>
2023-11-29 20:35:29 -0300

    .

--- Aprendendo/5 - Teste/js/main.js
+++ Aprendendo/5 - Teste/js/main.js
@@ -9,7 +9,7 @@ $('.slider-principal').slick({
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 1,
           slidesToScroll: 3,
           infinite: true,
           dots: true
@@ -18,7 +18,7 @@ $('.slider-principal').slick({
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 2
         }
       },
