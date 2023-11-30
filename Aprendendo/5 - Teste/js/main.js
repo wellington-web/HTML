9376ebb4c93624fefc4f36ba07cb339fdc3c492a
Wellington <wellingtonTgertrudes@gmail.com>
2023-11-30 19:54:23 -0300

    .

--- Aprendendo/5 - Teste/js/main.js
+++ Aprendendo/5 - Teste/js/main.js
@@ -3,20 +3,20 @@ $('.slider-principal').slick({
     dots: true,
     infinite: false,
     speed: 300,
-    slidesToShow: 1,
+    slidesToShow: 2,
     slidesToScroll: 3,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 2,
           slidesToScroll: 3,
           infinite: true,
           dots: true
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 500,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 2
@@ -31,6 +31,6 @@ $('.slider-principal').slick({
       }
       // You can unslick at a given breakpoint now by adding:
       // settings: "unslick"
-      // instead of a settings object
+      // instead of a settings objec
     ]
   });
