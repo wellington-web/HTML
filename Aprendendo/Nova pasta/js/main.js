49e434b452cddca7f733b091594414525a88eca7
Wellington <wellingtonTgertrudes@gmail.com>
2023-12-10 13:46:10 -0300

    slide

---
+++ Aprendendo/Nova pasta/js/main.js
@@ -0,0 +1,35 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings objec
+    ]
+  });
