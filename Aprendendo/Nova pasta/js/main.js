d92a84fba4e8d591a667cdaf69c801c698245128
Wellington <wellingtonTgertrudes@gmail.com>
2023-12-11 13:20:41 -0300

    slide

--- Aprendendo/Nova pasta/js/main.js
+++ Aprendendo/Nova pasta/js/main.js
@@ -2,13 +2,13 @@ $('.slider-principal').slick({
     dots: true,
     infinite: false,
     speed: 300,
-    slidesToShow: 2,
+    slidesToShow: 1,
     slidesToScroll: 3,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 3,
           infinite: true,
           dots: true
